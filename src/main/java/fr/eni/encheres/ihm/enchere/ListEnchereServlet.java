25a1231f7840c38cfc5946630dde07e9792eacc1
Sylvain DANIEL <sylvain.daniel2023@campus-eni.fr>
2023-05-09 10:28:18 +0200

    commit restant à priori ...

---
+++ src/main/java/fr/eni/encheres/ihm/enchere/ListEnchereServlet.java
@@ -0,0 +1,25 @@
+package fr.eni.encheres.ihm.enchere;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/enchere/list")
+public class ListEnchereServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: Liste enchères ").append(request.getContextPath());
+	}
+
+
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
